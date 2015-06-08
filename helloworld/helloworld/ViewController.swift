c13aa650a8aec94f69cf65da54cdfa40caebfb08
熊浩翔 <BearORC-vMac@xionghaoxiangdeMac.local>
2015-06-08 01:18:50 -0700

    init

---
+++ helloworld/helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by 熊浩翔 on 15/6/8.
+//  Copyright (c) 2015年 BearORC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
